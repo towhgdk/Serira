f59f572a336259539fed8843962c4a6bb850e531
towhgdk <jwb179@gmail.com>
2026-02-01 00:21:07 +0900

    이게 맞어?
    어케올리는지 모르겠어!

---
+++ Neo DirtyTest.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxRandom = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxUnit = new System.Windows.Forms.ComboBox();
+            this.radioButtonRead = new System.Windows.Forms.RadioButton();
+            this.radioButtonWrite = new System.Windows.Forms.RadioButton();
+            this.radioall = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioManual = new System.Windows.Forms.RadioButton();
+            this.buttonSaveGraph = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "테스트 시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(32, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "현재 속도: 0 MB/s";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "테스트할 드라이브 :";
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.DisplayScale = 0F;
+            this.formsPlot1.Location = new System.Drawing.Point(274, 31);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(500, 386);
+            this.formsPlot1.TabIndex = 6;
+            this.formsPlot1.Load += new System.EventHandler(this.formsPlot1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(32, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "테스트 중지!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // checkBoxRandom
+            // 
+            this.checkBoxRandom.AutoSize = true;
+            this.checkBoxRandom.Location = new System.Drawing.Point(32, 264);
+            this.checkBoxRandom.Name = "checkBoxRandom";
+            this.checkBoxRandom.Size = new System.Drawing.Size(178, 16);
+            this.checkBoxRandom.TabIndex = 9;
+            this.checkBoxRandom.Text = "랜덤쓰기 (읽기는 영향 없음)";
+            this.checkBoxRandom.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(5, 48);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBoxUnit
+            // 
+            this.comboBoxUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxUnit.FormattingEnabled = true;
+            this.comboBoxUnit.Items.AddRange(new object[] {
+            "GB",
+            "MB"});
+            this.comboBoxUnit.Location = new System.Drawing.Point(130, 48);
+            this.comboBoxUnit.Name = "comboBoxUnit";
+            this.comboBoxUnit.Size = new System.Drawing.Size(71, 20);
+            this.comboBoxUnit.TabIndex = 11;
+            this.comboBoxUnit.SelectedIndexChanged += new System.EventHandler(this.comboBoxUnit_SelectedIndexChanged);
+            // 
+            // radioButtonRead
+            // 
+            this.radioButtonRead.AutoSize = true;
+            this.radioButtonRead.Location = new System.Drawing.Point(9, 20);
+            this.radioButtonRead.Name = "radioButtonRead";
+            this.radioButtonRead.Size = new System.Drawing.Size(153, 16);
+            this.radioButtonRead.TabIndex = 12;
+            this.radioButtonRead.TabStop = true;
+            this.radioButtonRead.Text = "읽기 (관리자 권한 필요)";
+            this.radioButtonRead.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWrite
+            // 
+            this.radioButtonWrite.AutoSize = true;
+            this.radioButtonWrite.Location = new System.Drawing.Point(9, 45);
+            this.radioButtonWrite.Name = "radioButtonWrite";
+            this.radioButtonWrite.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonWrite.TabIndex = 13;
+            this.radioButtonWrite.TabStop = true;
+            this.radioButtonWrite.Text = "쓰기";
+            this.radioButtonWrite.UseVisualStyleBackColor = true;
+            this.radioButtonWrite.CheckedChanged += new System.EventHandler(this.radioButtonWrite_CheckedChanged);
+            // 
+            // radioall
+            // 
+            this.radioall.AutoSize = true;
+            this.radioall.Location = new System.Drawing.Point(126, 23);
+            this.radioall.Name = "radioall";
+            this.radioall.Size = new System.Drawing.Size(75, 16);
+            this.radioall.TabIndex = 14;
+            this.radioall.TabStop = true;
+            this.radioall.Text = "전체 용량";
+            this.radioall.UseVisualStyleBackColor = true;
+            this.radioall.CheckedChanged += new System.EventHandler(this.radioall_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonRead);
+            this.groupBox1.Controls.Add(this.radioButtonWrite);
+            this.groupBox1.Location = new System.Drawing.Point(32, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 67);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "테스트 선택";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioManual);
+            this.groupBox2.Controls.Add(this.radioall);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.comboBoxUnit);
+            this.groupBox2.Location = new System.Drawing.Point(32, 156);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(207, 78);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "용량 선택";
+            // 
+            // radioManual
+            // 
+            this.radioManual.AutoSize = true;
+            this.radioManual.Location = new System.Drawing.Point(6, 23);
+            this.radioManual.Name = "radioManual";
+            this.radioManual.Size = new System.Drawing.Size(75, 16);
+            this.radioManual.TabIndex = 15;
+            this.radioManual.TabStop = true;
+            this.radioManual.Text = "수동 입력";
+            this.radioManual.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveGraph
+            // 
+            this.buttonSaveGraph.Location = new System.Drawing.Point(274, 4);
+            this.buttonSaveGraph.Name = "buttonSaveGraph";
+            this.buttonSaveGraph.Size = new System.Drawing.Size(142, 23);
+            this.buttonSaveGraph.TabIndex = 17;
+            this.buttonSaveGraph.Text = "그래프 저장 (PNG)";
+            this.buttonSaveGraph.UseVisualStyleBackColor = true;
+            this.buttonSaveGraph.Click += new System.EventHandler(this.buttonSaveGraph_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSaveGraph);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBoxRandom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxRandom;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBoxUnit;
+        private System.Windows.Forms.RadioButton radioButtonRead;
+        private System.Windows.Forms.RadioButton radioButtonWrite;
+        private System.Windows.Forms.RadioButton radioall;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioManual;
+        private System.Windows.Forms.Button buttonSaveGraph;
+    }
+}
---
+++ Neo DirtyTest.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+using WinColor = System.Drawing.Color;
+using ScottColor = ScottPlot.Color;
+using WinFont = System.Drawing.Font;
+using WinFontStyle = System.Drawing.FontStyle;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ScottPlot.Plottables.Scatter scatter;
+        private List<double> xData = new List<double>();
+        private List<double> yData = new List<double>();
+        private bool isStopping = false;
+        private bool isRunning = false;
+        private Timer ledTimer = new Timer();
+        private int colorPhase = 0;
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool DeviceIoControl(SafeFileHandle hDevice, uint dwIoControlCode, IntPtr lpInBuffer, uint nInBufferSize, IntPtr lpOutBuffer, uint nOutBufferSize, out uint lpBytesReturned, IntPtr lpOverlapped);
+        const uint FSCTL_SET_SPARSE = 0x000900C4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ConnectStopButton();
+            SetupLEDLabel();
+        }
+
+        private void ConnectStopButton()
+        {
+            Control[] stopButtons = this.Controls.Find("buttonStop", true);
+            if (stopButtons.Length > 0) ((Button)stopButtons[0]).Click += ButtonStop_Click;
+            Control[] btn2 = this.Controls.Find("button2", true);
+            if (btn2.Length > 0) ((Button)btn2[0]).Click += ButtonStop_Click;
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            if (isRunning) { isStopping = true; if (label1 != null) label1.Text = "⛔ 중지 중..."; }
+        }
+
+        private void SetupLEDLabel()
+        {
+            if (label4 != null) { label4.Text = "제작 : DC 저장장치 갤러리\r\n무기를ㅡ접자 / Gemini"; label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter; label4.Font = new WinFont("맑은 고딕", 9, WinFontStyle.Bold); }
+            ledTimer.Interval = 100; ledTimer.Tick += (s, e) => {
+                if (label4 == null) return;
+                colorPhase = (colorPhase + 1) % 4;
+                label4.ForeColor = (colorPhase == 0) ? WinColor.DeepSkyBlue : (colorPhase == 2) ? WinColor.Blue : WinColor.DodgerBlue;
+            };
+            ledTimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            foreach (DriveInfo drive in DriveInfo.GetDrives()) { if (drive.IsReady) comboBox1.Items.Add($"{drive.VolumeLabel} ({drive.Name})"); }
+            if (comboBoxUnit.Items.Count == 0) { comboBoxUnit.Items.Add("GB"); comboBoxUnit.Items.Add("MB"); }
+            comboBoxUnit.SelectedIndex = 0;
+            radioall_CheckedChanged(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isRunning) return;
+            if (comboBox1.SelectedItem == null) { MessageBox.Show("드라이브를 선택해주세요!"); return; }
+
+            bool isReadMode = (radioButtonRead != null && radioButtonRead.Checked);
+            bool isRandomMode = checkBoxRandom != null && checkBoxRandom.Checked;
+            bool isAllMode = (radioall != null && radioall.Checked);
+
+            isRunning = true; isStopping = false;
+            button1.Enabled = false; groupBox1.Enabled = false; groupBox2.Enabled = false;
+
+            xData.Clear(); yData.Clear(); formsPlot1.Plot.Clear();
+
+            string selectedDrive = comboBox1.SelectedItem.ToString();
+            string driveLetter = selectedDrive.Substring(selectedDrive.LastIndexOf('(') + 1, 1);
+            long totalBytes = 0;
+            try
+            {
+                DriveInfo dInfo = new DriveInfo($@"{driveLetter}:\");
+                totalBytes = isAllMode ? (isReadMode ? dInfo.TotalSize : dInfo.AvailableFreeSpace - (300 * 1024 * 1024)) : (long)numericUpDown1.Value * (comboBoxUnit.Text == "GB" ? 1024L * 1024 * 1024 : 1024L * 1024);
+            }
+            catch { isRunning = false; return; }
+
+            int totalMB = (int)(totalBytes / 1024 / 1024);
+            string path = isReadMode ? $@"\\.\{driveLetter}:" : Path.Combine($@"{driveLetter}:\", "DensityTest.bin");
+            int bufferSize = (totalMB <= 32768) ? 4 * 1024 * 1024 : 32 * 1024 * 1024;
+            byte[] buffer = new byte[bufferSize];
+            Stopwatch sw = new Stopwatch(); Random rnd = new Random();
+
+            // 1. 색상 변수 선언 (에러 해결 핵심)
+            byte r = (byte)rnd.Next(60, 190);
+            byte g = (byte)rnd.Next(60, 190);
+            byte b = (byte)rnd.Next(60, 190);
+
+            // 2. 타이틀 및 축 레이블 복구
+            string modeStr = isReadMode ? "Raw Read" : "Write";
+            string typeStr = isRandomMode ? "Random" : "Sequential";
+            formsPlot1.Plot.Title($"{typeStr} {modeStr} (Block: {bufferSize / 1024 / 1024}MB)");
+            formsPlot1.Plot.XLabel("Capacity (MB)");
+            formsPlot1.Plot.YLabel("Speed (MB/s)");
+
+            scatter = formsPlot1.Plot.Add.Scatter(xData, yData);
+
+            // 3. 점과 선 색상 일치화 (투명도 조절로 안개 효과 극대화)
+            scatter.MarkerColor = new ScottColor(r, g, b, 180);
+            scatter.LineColor = new ScottColor(r, g, b, 40); // 선은 연하게
+            scatter.MarkerSize = 5;
+            scatter.LineWidth = 1;
+
+            try
+            {
+                FileStream fs = isReadMode ? new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, bufferSize, FileOptions.WriteThrough)
+                                           : new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize, FileOptions.WriteThrough);
+                if (!isReadMode && isRandomMode)
+                {
+                    uint outB; DeviceIoControl(fs.SafeFileHandle, FSCTL_SET_SPARSE, IntPtr.Zero, 0, IntPtr.Zero, 0, out outB, IntPtr.Zero);
+                    fs.SetLength(totalBytes); rnd.NextBytes(buffer);
+                }
+                else if (!isReadMode) Array.Clear(buffer, 0, buffer.Length);
+
+                using (fs)
+                {
+                    int loops = (int)(totalBytes / bufferSize);
+                    for (int i = 0; i < loops; i++)
+                    {
+                        if (isStopping) break;
+
+                        // (중략: 랜덤 모드 Seek 및 실제 Read/Write 로직)
+                        if (isRandomMode) fs.Seek((long)(rnd.NextDouble() * (totalBytes / bufferSize)) * bufferSize, SeekOrigin.Begin);
+
+                        sw.Restart();
+                        if (isReadMode) fs.Read(buffer, 0, buffer.Length); else fs.Write(buffer, 0, buffer.Length);
+                        sw.Stop();
+
+                        double mb = (i + 1) * (bufferSize / 1024.0 / 1024.0);
+                        double spd = (bufferSize / 1024.0 / 1024.0) / sw.Elapsed.TotalSeconds;
+
+                        xData.Add(mb);
+                        yData.Add(spd);
+
+                        label1.Text = $"진행: {mb:F0}/{totalMB} MB | {spd:F0} MB/s";
+
+                        // --- 바로 이 지점에 삽입하세요! ---
+                        // 4MB 단위(4194304)일 때는 25번마다, 32MB 단위일 때는 10번마다 갱신
+                        int refreshRate = (bufferSize == 4 * 1024 * 1024) ? 25 : 10;
+
+                        if (i % refreshRate == 0 || i == loops - 1)
+                        {
+                            formsPlot1.Plot.Axes.AutoScale();
+                            formsPlot1.Plot.Axes.SetLimits(bottom: 0);
+                            formsPlot1.Refresh();
+                        }
+                        // --------------------------------
+
+                        Application.DoEvents();
+                    }
+                }
+                if (!isReadMode && File.Exists(path)) File.Delete(path);
+                MessageBox.Show(isStopping ? "중지됨" : "완료!");
+            }
+            catch (Exception ex) { MessageBox.Show("에러: " + ex.Message); }
+            finally
+            {
+                isRunning = false; button1.Enabled = true; groupBox1.Enabled = true; groupBox2.Enabled = true;
+                radioall_CheckedChanged(null, null); label1.Text = "대기 중...";
+            }
+        }
+
+        private void radioall_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isAll = radioall != null && radioall.Checked;
+            if (numericUpDown1 != null) numericUpDown1.Enabled = !isAll;
+            if (comboBoxUnit != null) comboBoxUnit.Enabled = !isAll;
+        }
+
+        private void buttonSaveGraph_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog { Filter = "PNG|*.png", FileName = $"Result_{DateTime.Now:HHmmss}.png" };
+            if (s.ShowDialog() == DialogResult.OK) { formsPlot1.Plot.SavePng(s.FileName, 1920, 1080); MessageBox.Show("FHD 저장 완료!"); }
+        }
+
+        private void radioButtonRead_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxRandom != null) checkBoxRandom.Text = radioButtonRead.Checked ? "랜덤 읽기 (영향 없음)" : "랜덤 쓰기";
+        }
+
+        // 껍데기 함수들
+        private void button2_Click(object sender, EventArgs e) => ButtonStop_Click(sender, e);
+        private void formsPlot1_Load(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) { }
+        private void radioButtonWrite_CheckedChanged(object sender, EventArgs e) { }
+        private void comboBoxUnit_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void groupBox2_Enter(object sender, EventArgs e) { }
+    }
+}
